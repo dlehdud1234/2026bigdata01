6f24772773bb26eac0df7c966a0427acc567615b
dlehdud <as007564@gmail.com>
2026-04-02 16:25:38 +0900

    week05_amusementpark_final

--- week05.py
+++ week05.py
@@ -8,5 +8,5 @@ for i in range(humans):
     age = int(input("나이 : "))
     ages.append(age)
 
-print(f"총 요금 {ticket.entrace_fee(ages)}원 입니다")
+print(f"총 요금 {ticket.Entrance_Fee(ages)}원 입니다")
 #print(ages)
